5a5d2d0d578829ce43fcfbe6c8a723eacc883d7f
pavlo.zaloznyi <pavlo.zaloznyi@pm.bet>
2022-08-28 20:19:39 +0300

    initial template

---
+++ src/main/java/com/homework/cursorshops/CursorShopsApplication.java
@@ -0,0 +1,13 @@
+package com.homework.cursorshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorShopsApplication.class, args);
+	}
+
+}
